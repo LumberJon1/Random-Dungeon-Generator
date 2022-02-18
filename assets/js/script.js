1a7cbf2c763fab5241ef5cedc3a7f07fdce2992d
Jonathan W <jwilliams12117@gmail.com>
2022-02-17 19:14:45 -0700

    Replicated loops with position and left/top styles and divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,31 @@ const activeColor = "#e9d9b0";
 const inactiveColor = "white";
 
 //Resize based on hardcoded parameters for now, eventually user input
-dungeonGrid.setAttribute("width", "500");
-dungeonGrid.setAttribute("height", "500");
+dungeonGrid.style.width = "500px";
+dungeonGrid.style.height = "500px";
+
+//Convert style elements from pixels to ints so they can drive other values
+const gridWidth = (dungeonGrid.style.width).split("px")[0];
+const gridHeight = (dungeonGrid.style.height).split("px")[0];
 
 //Define tile width and height based on dungeonGrid proportions
-const tileWidth = dungeonGrid.getAttribute("width") / numTiles;
-const tileHeight = dungeonGrid.getAttribute("height") / numTiles;
+const tileWidth = gridWidth / numTiles;
+const tileHeight = gridHeight / numTiles;
 
 //fill the canvas
 for (let i = 0; i < numTiles; i++) {
     let col = 0;
     for (let j = 0; j < numTiles; j++) {
         console.log("row "+(i + 1)+", column "+(col + 1));
-        //ctx.strokeRect((col * tileWidth), (i * tileHeight), tileWidth, tileHeight);
+        let tile = document.createElement("div");
+        tile.style.border = "1px solid black";
+        tile.style.position = "absolute";
+        tile.style.left = (col * tileWidth)+"px";
+        tile.style.top = (i * tileHeight)+"px";
+        tile.style.width = (tileWidth)+"px";
+        tile.style.height = (tileHeight)+"px";
+        dungeonGrid.appendChild(tile);
+        console.log(tile.style.left, tile.style.top);
         col ++;
     }
 }
